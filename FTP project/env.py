6813dbcacfc08353d7d91b9195c2aeb6d0d983aa
Alexanderlau <429095816@qq.com>
2016-11-25 23:13:37 +0800

    FTP

---
+++ FTP project/env.py
@@ -0,0 +1,14 @@
+import ftplib
+import os
+import socket
+
+
+HOST = 'ftp.mozilla.org'
+DIRN = 'pub/mozilla.org/webtools'
+FILE = 'bugzilla-LATEST.tar.gz'
+
+def main():
+    try:
+        f = ftplib.FTP(HOST)
+    except(socket.error,socket.gaierror):
+        print()

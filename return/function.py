0f0e6069fe44b12136dffedd06ce58d380c3092f
Alexanderlau <429095816@qq.com>
2016-12-13 22:51:38 +0800

    project

---
+++ return/function.py
@@ -0,0 +1,8 @@
+def break_words(stuff):
+    """This function will break uo words for us."""
+    words = stuff.spilt(' ')
+    return words
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+

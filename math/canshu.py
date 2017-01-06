3a918440e796f97e01224b9dbd78b115e3c8a78a
Alexanderlau <429095816@qq.com>
2017-01-06 18:58:10 +0800

    python-project

---
+++ math/canshu.py
@@ -0,0 +1,7 @@
+def try_to_change():
+    a = int(input("Please input a number:"))
+def try_to_changes():
+    b = int(input("Please input a number:"))
+if __name__ == "__main__":
+    d = try_to_change() + try_to_changes()
+    print(d)

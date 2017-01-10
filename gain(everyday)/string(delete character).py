59aaab5bc8701cacdc50c7e9167151ca2eb93442
Alexanderlau <429095816@qq.com>
2017-01-10 22:18:15 +0800

    spider

--- gain(everyday)/string(delete character).py
+++ gain(everyday)/string(delete character).py
@@ -1,35 +1,54 @@
-a = str(1230403012)
-#找到第一个字符
-b = a[0]
-#删除第一个字符
-a = a.strip(b)
-print(a)
-#判断第一个数是否是0
-if b == 0:
-    print('Yes')
-else:
-    print('No')
-#判断字符串长度
-if len(a) > 5:
-    print('Long')
-else:
-    print('short')
-#判断字符串前几位是否对应要求
-t = input('Enter str')
-x = str(t)
-c = x[0]
-d = x[1]
-if len(x) >= 5:
-    if c and d == '0':
-        print('Never!')
-    elif c == '0':
-        print('not good!')
-    elif c != 0:
-        print('Good!')
-    else:
-        print('i dont know')
-else:
-    print('good')
+# a = str(1230403012)
+# #找到第一个字符
+# b = a[0]
+# #删除第一个字符
+# a = a.strip(b)
+# print(a)
+# #判断第一个数是否是0
+# if b == 0:
+#     print('Yes')
+# else:
+#     print('No')
+# #判断字符串长度
+# if len(a) > 5:
+#     print('Long')
+# else:
+#     print('short')
+# #判断字符串前几位是否对应要求
+# t = input('Enter str')
+# x = str(t)
+# if len(x) >= 5:
+#     c = x[0]
+#     d = x[1]
+#     if c and d == '0':
+#         print('Never!')
+#     elif c == '0':
+#         print('not good!')
+#     elif c != 0:
+#         print('Good!')
+#     else:
+#         print('i dont know')
+# else:
+#     print('good')
 #替换字符串内的数
+# num = input('Enter a number')
+# a = num[0]
+# b = num[1]
+# c = num[2]
+# if len(num) >= 5:
+#     if a == '0' and b == '0':
+#         d = num.replace(b,'')
+#         print(d)
+#     else:
+#         print(num)
+# else:
+#     print(num)
+# #去掉字符串空格
+# num = input('enter number:')
+# a = str(num)
+# print(a)
+# b = a.split()
+# print(b)
+
 
 

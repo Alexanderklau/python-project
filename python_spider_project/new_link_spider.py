7c62026f953d9ef15818967c49dd960c0a5e3f63
Alexanderklau <429095816@qq.com>
2017-09-25 09:29:14 +0800

    add_spier

--- python_spider_project/new_link_spider.py
+++ python_spider_project/new_link_spider.py
@@ -3,6 +3,8 @@ __author__ = 'yemilice_lau'
 import urllib2
 import re
 import urlparse
+from datetime import datetime
+import time
 
 # 获取到网页并且下载下来(urllib2)
 """
@@ -10,10 +12,14 @@ import urlparse
 """
 
 
-def download(url, user_agent='wswp', num_retries=2):
+def download(url, user_agent='wswp', proxy=None, num_retries=2):
     print 'Downloading:', url
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers=headers)
+    opener = urllib2.build_opener()
+    if proxy:
+        proxy_parms = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_parms))
     try:
         html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
@@ -21,7 +27,7 @@ def download(url, user_agent='wswp', num_retries=2):
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, user_agent, num_retries - 1)
+                return download(url, user_agent, proxy, num_retries - 1)
     return html
 
 
@@ -52,3 +58,19 @@ def linl_crawler(seed_url, link_regex):
 
 
 linl_crawler('http://example.webscraping.com/places/default', '/(index|view)')
+
+
+class Throottle:
+    def __init__(self, delay):
+        self.delay = delay
+        self.domains = {}
+
+    def wait(self, url):
+        domain = urlparse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
